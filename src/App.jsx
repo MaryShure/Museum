a5b6ce4af498542ba5ea347336ffa4d8c0e7c4d3
MaryShure <bloodbunny6663@gmail.com>
2026-04-26 22:14:12 +0300

    scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import "./styles/global.css";
-import "./styles/main_page.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import MainPage from "./pages/MainPage";
-import TimeTableAndPricePage from "./pages/TimeTableAndPricePage";
 import ServicesPage from "./pages/ServicesPage";
+import TimeTableAndPricePage from "./pages/TimeTableAndPricePage";
 
 function App() {
   return (
     <HashRouter>
+      <ScrollToTop />
       <Header />
 
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/timeprice" element={<TimeTableAndPricePage />} />
         <Route path="/services" element={<ServicesPage />} />
+        <Route path="/timeprice" element={<TimeTableAndPricePage />} />
       </Routes>
 
       <Footer />
